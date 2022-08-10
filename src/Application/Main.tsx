e58772e43416f91afa8b85ecc922279670da216d
avivash <av@andrewvivash.com>
2022-08-10 16:36:11 -0700

    Chore: add a couple more comments

--- src/Application/Main.tsx
+++ src/Application/Main.tsx
@@ -6,14 +6,17 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App"
 
+/**
+ * I've left the testnet chains commented out here in case we want to test with them
+ */
 const { chains, provider } = configureChains(
   [chain.mainnet/*, chain.ropsten, chain.rinkeby, chain.goerli*/],
   [publicProvider()]
 );
 
 /**
- * I'm limiting the available wallets to MetaMask for now, since it's the only
- * one that supports the RPC methods we need
+ * I'm limiting the available wallets to MetaMask for now, since it's the only one that
+ * supports the RPC methods we need(`eth_decrypt` and `eth_getEncryptionPublicKey`)
  */
 const connectors = connectorsForWallets([
   {
