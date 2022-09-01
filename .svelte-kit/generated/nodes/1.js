10abea983c0cdd6e76e8b8a4ceb85048bbdb1d30
avivash <av@andrewvivash.com>
2022-09-01 17:42:21 -0400

    Fix: remove .svelte-kit/ from previous push

--- .svelte-kit/generated/nodes/1.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../node_modules/.pnpm/@sveltejs+kit@1.0.0-next.431_fhgkwirxvgjw2igda6zmhiisma/node_modules/@sveltejs/kit/src/runtime/components/error.svelte";
--- .svelte-kit/types/src/routes/gallery/$types.d.ts
+++
@@ -1,7 +0,0 @@
-import type * as Kit from '@sveltejs/kit';
-
-interface RouteParams extends Partial<Record<string, string>> {}
-
-export type Errors = undefined;
-export type PageData = import('../$types.js').LayoutData;
-export type PageServerData = null;
