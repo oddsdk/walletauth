e84ba2247350b9d6b88878b1ed9ce2d60b9d8c09
avivash <av@andrewvivash.com>
2022-09-07 12:53:09 -0700

    Fix: adjust <Connect /> width

--- src/components/auth/connect/Connect.svelte
+++ src/components/auth/connect/Connect.svelte
@@ -11,7 +11,7 @@
     class="hidden md:flex w-max max-w-[150px] cursor-pointer justify-between items-center rounded-full transition-colors duration-250 ease-in-out bg-gray-buttonDark dark:bg-gray-buttonLight border border-gray-900 dark:border-gray-50 group text-sm py-2 px-2"
   >
     <span
-      class="overflow-hidden text-ellipsis w-[calc(100%-10px)] inline-block"
+      class="overflow-hidden text-ellipsis w-full inline-block"
       >{$sessionStore?.address}</span
     >
   </div>
