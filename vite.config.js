2b3e22c1ab7532317291398e2be39e8682ee8f9b
avivash <av@andrewvivash.com>
2023-02-10 15:27:34 -0800

    Feat: enable sourcemaps in vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
   build: {
+    sourcemap: true,
     target: 'es2020'
   },
   plugins: [sveltekit()],
